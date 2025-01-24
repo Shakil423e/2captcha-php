a8bf626158e42e6500d4899d227b8a525e55b181
Shakil423e <shakile301@gmail.com>
2025-01-24 12:28:34 +0600

    Update atb_captcha.php

--- examples/atb_captcha.php
+++ examples/atb_captcha.php
@@ -4,13 +4,13 @@ set_time_limit(130);
 
 require(__DIR__ . '/../src/autoloader.php');
 
-$solver = new \TwoCaptcha\TwoCaptcha('YOUR_API_KEY');
+$solver = new \TwoCaptcha\TwoCaptcha('0ef3026334a4c92e9184cf7bc32fd266');
 
 try {
     $result = $solver->atb_captcha([
         'sitekey'     => 'af23e041b22d000a11e22a230fa8991c',
         'api_server' => 'https://cap.aisecurius.com',
-        'url'        => 'https://example.com/',
+        'url'        => 'https://2captcha.com/cabinet',
     ]);
 } catch (\Exception $e) {
     die($e->getMessage());
